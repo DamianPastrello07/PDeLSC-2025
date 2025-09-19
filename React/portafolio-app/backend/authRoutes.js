583fe12ee083549bf6d17f00c6954d04402de9bf
DamianPastrello07 <damianpastrello@gmail.com>
2025-09-19 11:25:55 -0300

    .

--- React/portafolio-app/backend/authRoutes.js
+++ React/portafolio-app/backend/authRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 
 const router = express.Router();
-const SECRET = process.env.JWT_SECRET || "secreto123";
+const SECRET = process.env.JWT_SECRET || "supersecreto123";
 
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
